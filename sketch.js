e02ab318b64c782fef870beabc5595c2a741291a
aevaeva <130646728+aevaeva@users.noreply.github.com>
2023-05-31 20:45:47 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,96 +1,58 @@
-let object = [];
-let objectNum = 20;
-
+var balls=[]
+var mic
+var s
+var vid
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	for (let i = 0; i < objectNum; i++) {
-		object.push(new drawObject());
-	}
+	createCanvas(windowWidth, windowHeight,WEBGL);
+	mic = new p5.AudioIn()
+	vid = createCapture(VIDEO)
+	vid.size(width, height);
+  vid.hide();
+	mic.start()
+	background(0, 0, 0);
+	
+	
 }
 
 function draw() {
-	blendMode(BLEND);
-	background(0);
-	for (let i = 0; i < objectNum; i++) {
-		object[i].update(i);
-		object[i].display();
-	}
-	// drawGrid(25);
+	var vol = mic.getLevel();
+  s = map(vol, 0, 0.5, 30, 230);
+  background(0, 0, 0); 
+  for (b of balls) {
+    b.update();
+  }
 }
 
-class drawObject {
-	constructor() {
-		this.sizeList = [100, 250, 500];
-		this.objectSize = this.sizeList[int(random(this.sizeList.length))];
-		this.motion;
-		this.counter = 0;
-		this.counter2 = 0;
-		this.interval = this.counter + 1.3;
-		this.initPosX = width / 2;
-		this.desX = this.objectSize / 2 * int(random(1, width / this.objectSize * 2));
-		this.distanceX = this.desX - this.initPosX;
-		this.initPosY = height / 2;
-		this.desY = this.objectSize / 2 * int(random(1, height / this.objectSize * 2));
-		this.distanceY = this.desY - this.initPosY;
-		this.objectColor = 255;
-		this.speed = 0.01 * int(random(1, 2));
-		this.randomSelector = int(random(0, 3));
-		this.randomTex = int(random(0, 10));
-		textAlign(CENTER, CENTER);
-		textFont('helvetica');
-		rectMode(CENTER);
-	}
-	display() {
-		blendMode(DIFFERENCE);
-		noStroke();
-		fill(this.objectColor);
-		textSize(this.objectSize);
-		switch (this.randomSelector) {
-			case 0:
-				text(this.randomTex, this.x, this.y);
-				break;
-			case 1:
-				rect(this.x, this.y, this.objectSize);
-				break;
-			default:
-				ellipse(this.x, this.y, this.objectSize);
-		}
-	}
-	update() {
-		this.motion = easeOutExpo(this.counter);
-		this.x = int(this.initPosX + this.distanceX * this.motion);
-		this.y = int(this.initPosY + this.distanceY * this.motion);
-		if (this.counter < 1) {
-			this.counter += this.speed;
-			this.counter2 += this.speed;
-		} else if (this.counter2 < this.interval) {
-			this.counter2 += this.speed;
-		} else {
-			this.randomSelector = int(random(0, 3));
-			this.counter = 0;
-			this.counter2 = 0;
-			this.speed = 0.01;
-			this.initPosX = this.x;
-			this.desX = this.objectSize / 2 * int(random(1, width / this.objectSize * 2));
-			this.distanceX = this.desX - this.initPosX;
-			this.initPosY = this.y;
-			this.desY = this.objectSize / 2 * int(random(1, height / this.objectSize * 2));
-			this.distanceY = this.desY - this.initPosY;
-		}
-	}
+function mouseDragged(){
+	balls.push(new Ball(mouseX,mouseY))
 }
 
-function easeOutExpo(x) {
-	return x === 1 ? 1 : 1 - pow(2, -10 * x);
-}
+class Ball {
+  constructor(x, y) {
+    this.x = x - width / 2;
+    this.y = y - height / 2;
+    this.z = random(-30, 30);
+    this.dx = random(9) - 4;
+    this.dy = random(9) - 4;
+    this.dz = random(9) - 4;
+    this.c = color(random(255), random(255), random(255));
+    this.size = random(60, 100);
+  }
 
-function drawGrid(gridSize) {
-	stroke(255);
-	strokeWeight(1);
-	for (let x = 0; x <= 1000; x += gridSize) {
-		for (let y = 0; y <= 1000; y += gridSize) {
-			line(0, y, width, y);
-		}
-		line(x, 0, x, height);
-	}
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+    this.z += this.dz;
+
+    let zoomFactor = map(mouseX, 0, width, 0.5, 2); // Adjust the zoom factor based on mouseX position
+    let scaledSize = this.size * zoomFactor;
+
+    push();
+    translate(this.x, this.y, this.z);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    texture(vid);
+    box(scaledSize, scaledSize, scaledSize);
+    pop();
+  }
 }
